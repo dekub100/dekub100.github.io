5f5b52b17f279305ecc2659257a0d90b1a69cad3
dekub100 <kuba911007@gmail.com>
2024-05-04 16:39:36 +0200

    updates: - added navigation menu - changed logo sizing for mobile - renamed the polish file site - added a slight animation on the links
    +(testing with the links so it doesnt show .html in the link)

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-document.querySelector('.discord').addEventListener('click', function() {
-    var textArea = document.createElement('textarea');
-    textArea.value = this.getAttribute('data-clipboard-text');
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textArea);
-    var notification = this.parentNode.querySelector('.copy-notification');
-    notification.style.display = 'block';
-    // Reset animation properties before applying them again
-    notification.style.animation = 'none';
-    notification.offsetHeight; /* trigger reflow */
-    notification.style.animation = null; 
-    setTimeout(function() {
-    notification.style.animation = 'fadeOut 0.25s ease-out'; // Apply fade-out animation
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.discord').addEventListener('click', function() {
+        var textToCopy = 'dekub100'; // Change this to the text you want to copy
+        var textArea = document.createElement('textarea');
+        textArea.value = textToCopy;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+        
+        var notification = this.parentNode.querySelector('.copy-notification');
+        notification.style.display = 'block';
+        // Reset animation properties before applying them again
+        notification.style.animation = 'none';
+        notification.offsetHeight; /* trigger reflow */
+        notification.style.animation = null; 
         setTimeout(function() {
-            notification.style.display = 'none'; // Hide notification after animation completes
-        }, 250); // Fade out duration: 250ms
-    }, 2000); // Hide notification after 2 seconds
+            notification.style.animation = 'fadeOut 0.25s ease-out'; // Apply fade-out animation
+            setTimeout(function() {
+                notification.style.display = 'none'; // Hide notification after animation completes
+            }, 250); // Fade out duration: 250ms
+        }, 2000); // Hide notification after 2 seconds
+    });
 });
 
 
@@ -34,3 +38,10 @@ document.querySelector('.discord').addEventListener('click', function() {
     var tooltip = this.parentNode.querySelector('.tooltip');
     tooltip.style.display = 'none';
 });
+
+function toggleMobileNav() {
+    var navButton = document.querySelector('.mobile-nav-button');
+    navButton.classList.toggle('open');
+    var nav = document.querySelector('.mobile-nav');
+    nav.classList.toggle('mobile-nav-open');
+}
